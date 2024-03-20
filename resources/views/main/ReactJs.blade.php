3789e2afb7c90645bc3f5bc783be1877f931d095
Pourya Mahdavi Abdar <78024892+prymhdv@users.noreply.github.com>
2024-03-20 00:23:50 -0700

    first commit 2024 03 20 c

--- resources/views/main/ReactJs.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.master')
-@section('content')
-<!-- React JS -->
-
-<script>
-console.log("-------------------Scraping links on current page  (ReactJs.blade) --------------------------");
-</script>
-@endsection
