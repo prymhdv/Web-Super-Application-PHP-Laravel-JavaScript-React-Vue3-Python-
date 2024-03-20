b4065a325be5a785a9e5a4e310bab62f151c8d00
Pourya Mahdavi Abdar <78024892+prymhdv@users.noreply.github.com>
2024-03-20 09:44:08 -0700

    first commit 2024 03 20 c

--- resources/js/Vite/React-appTest/app2.jsx
+++
@@ -1,8 +0,0 @@
-// resources/js/components/HelloReact.js
-
-// resoureces/js/app.js
-
-require('./bootstrap');
-
-// React Components
-require('./components/HelloReact')
--- resources/js/Vite/Vue3-app3/services/ability.js
+++
@@ -1,5 +0,0 @@
-import { AbilityBuilder, createMongoAbility } from '@casl/ability'
-
-const { can, cannot, build } = new AbilityBuilder(createMongoAbility);
-
-export default build();
